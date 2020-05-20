505868dfc5a9732545505d84cc31b56ff1377b16
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-05-20 16:34:14 +0300

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./CSS/main.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Home from "./Components/React/home";
 import Nav from "./Components/React/navbar";
 import About from "./Components/React/aboutApp.js";
@@ -10,7 +15,7 @@ import HamburgerMenu from "./Components/React/hamburgerMenu";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <div>
         <Nav />
         <HamburgerMenu />
@@ -19,10 +24,10 @@ function App() {
           <Route path="/home" exact component={Home} />
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
-          <Route path="//countries/:name" component={CountryDetail} />
+          <Route path="/countries/:name" component={CountryDetail} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
