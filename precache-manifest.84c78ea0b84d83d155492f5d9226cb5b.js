bbad2c7054372559e96358552a97e89a281b72ce
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-05-20 15:44:09 +0300

    Updates

---
+++ precache-manifest.84c78ea0b84d83d155492f5d9226cb5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e17af6facd3954e3bb381282a64712b",
+    "url": "/Countries-app/index.html"
+  },
+  {
+    "revision": "402276b682fe1923a2c9",
+    "url": "/Countries-app/static/css/main.001df1d3.chunk.css"
+  },
+  {
+    "revision": "6f34256d7ae96b3d5393",
+    "url": "/Countries-app/static/js/2.9ce729fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Countries-app/static/js/2.9ce729fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402276b682fe1923a2c9",
+    "url": "/Countries-app/static/js/main.c1543a9b.chunk.js"
+  },
+  {
+    "revision": "9c83c02649c2fe020151",
+    "url": "/Countries-app/static/js/runtime-main.a77276aa.js"
+  }
+]);
